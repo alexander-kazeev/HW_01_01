382459f06d60fffae7d677e4cf841c60170c3fe4
alexander-kazeev <32892445+alexander-kazeev@users.noreply.github.com>
2017-10-20 10:05:37 +0300

    Use common library

--- L01.1.1-maven/src/main/java/ru/otus/aik/l0111/Main.java
+++ L01.1.1-maven/src/main/java/ru/otus/aik/l0111/Main.java
@@ -1,7 +1,9 @@
 package ru.otus.aik.l0111;
 
+import com.google.common.base.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ура! Заработало!");
+        System.out.println(Joiner.on(", ").join("Ура", "заработало!"));
     }
 }
